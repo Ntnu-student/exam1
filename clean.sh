b8fd27112b5682039cb723c725d01c78e2473a77
Ntnu-student <marsbj@stud.ntnu.no>
2023-12-09 21:27:36 +0100

    Removed the files that are not in use
    Removed the variables for files not generated by C-deploy.sh

--- clean.sh
+++ clean.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 # Cleaning script
 # Assigning files to clean up into variables
-file1="kommune.txt"
-file2="kommune-online.txt"
-file3="kommune-newline.txt"
-file4="kommune-tableonly.txt"
-file5="kommuner.html"
-file6="kommune-table.txt"
-file7="data.txt"
-file8="column2.txt"
-file9="column5.txt"
-file10="column2_5.txt"
-file11="done.html"
-file12="coordata.txt"
-file13="wiki.list.no.newlines.html"
-file14="table.txt"
-file15="wikitable.html"
-file16="wikilist.html"
-file17="data.html"
+file1="decimal-lat.txt"
+file2="decimal-lon.txt"
+file3="merged-coords.txt"
+file4="data.txt"
+file5="column2.txt"
+file6="column5.txt"
+file7="done.html"
+file8="coordata.txt"
+file9="wiki.list.no.newlines.html"
+file10="table.txt"
+file11="wikitable.html"
+file12="wikilist.html"
+file13="data.html"
 
 
 # Delete files
-rm -f "$file1" "$file2" "$file3" "$file4" "$file5" "$file6" "$file7" "$file8" "$file9" "$file10" "$file11" "$file12" "$file13" "$file14" "$file15" "$file16" "$file17"
+rm -f "$file1" "$file2" "$file3" "$file4" "$file5" "$file6" "$file7" "$file8" "$file9" "$file10" "$file11" "$file12" "$file13"
 
 echo "Files has been cleaned up. :D"
 
